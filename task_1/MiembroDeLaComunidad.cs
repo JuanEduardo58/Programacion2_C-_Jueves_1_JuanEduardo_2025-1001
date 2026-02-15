0367789ad430b7a6dbcae2a51d73e4193fd2ba90
JuanEduardo58 <20251001@itla.edu.do>
2026-02-14 23:41:42 -0400

    adding my first task

---
+++ task_1/MiembroDeLaComunidad.cs
@@ -0,0 +1,7 @@
+﻿namespace SistemaComunidad;
+
+public abstract class MiembroDeLaComunidad
+{
+    public string Nombre { get; set; }
+    public virtual void Presentarse() => Console.WriteLine($"Soy {Nombre}, miembro de la comunidad.");
+}
