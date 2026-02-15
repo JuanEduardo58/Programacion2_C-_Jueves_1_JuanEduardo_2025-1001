abdb95dfd91874f847b5a8694ee54186d033dadd
JuanEduardo58 <20251001@itla.edu.do>
2026-02-14 23:36:31 -0400

    Delete MiembroDeLaComunidad.cs

--- MiembroDeLaComunidad.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SistemaComunidad;
-
-public abstract class MiembroDeLaComunidad
-{
-    public string Nombre { get; set; }
-    public virtual void Presentarse() => Console.WriteLine($"Soy {Nombre}, miembro de la comunidad.");
-}
